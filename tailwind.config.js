db0da1971274d7ea122e4a102d154e46695dd040
painisreal <ariknesh05@gmail.com>
2024-03-20 21:42:31 +0530

    now last new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./space-btr/index.html","./nav.js"],
+  content: ["./index.html","./nav.js"],
   theme: {
     extend: {},
   },
