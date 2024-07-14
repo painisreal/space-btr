e8e9993341eff2ba82a5a35a8f4e5b4d9503bfe2
painisreal <ariknesh05@gmail.com>
2024-07-14 16:29:17 +0530

    !fix: explore,about

--- docs/abt.js
+++ docs/abt.js
@@ -1,7 +1,7 @@
 var abt_card = document.getElementById("abt_card_id"); // ?side-menu-bar
 function openabt() {
   abt_card.style.display = "block";
-  abt_card.style.position = "relative";
+  abt_card.style.position = "absolute";
   
 }
 function closeabt() {
